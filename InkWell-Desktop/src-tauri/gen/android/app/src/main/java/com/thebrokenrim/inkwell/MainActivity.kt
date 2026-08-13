cd06d6c7d3148806c307ad3e5ffe758320541a16
CYB3RG3CKO <50240279+cyb3rg3cko@users.noreply.github.com>
2026-08-12 22:02:35 -0400

    Added signed android files

---
+++ InkWell-Desktop/src-tauri/gen/android/app/src/main/java/com/thebrokenrim/inkwell/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thebrokenrim.inkwell
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}

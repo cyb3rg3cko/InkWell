a8f405978e966e66d5212af9a0359753a7c9f942
CYB3RG3CKO <50240279+cyb3rg3cko@users.noreply.github.com>
2026-08-13 13:54:22 -0400

    Update build.gradle.kts

--- InkWell-Desktop/src-tauri/gen/android/app/build.gradle.kts
+++ InkWell-Desktop/src-tauri/gen/android/app/build.gradle.kts
@@ -1,18 +1,17 @@
 import java.util.Properties
+import java.io.FileInputStream
 
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("rust")
 }
-
 val tauriProperties = Properties().apply {
     val propFile = file("tauri.properties")
     if (propFile.exists()) {
         propFile.inputStream().use { load(it) }
     }
 }
-
 android {
     compileSdk = 36
     namespace = "com.thebrokenrim.inkwell"
@@ -23,6 +22,19 @@ android {
         targetSdk = 36
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
+    }
+    signingConfigs {
+        create("release") {
+            val keystorePropertiesFile = rootProject.file("keystore.properties")
+            val keystoreProperties = Properties()
+            if (keystorePropertiesFile.exists()) {
+                keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+            }
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["password"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["password"] as String
+        }
     }
     buildTypes {
         getByName("debug") {
@@ -38,6 +50,7 @@ android {
             }
         }
         getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             proguardFiles(
                 *fileTree(".") { include("**/*.pro") }
@@ -53,11 +66,9 @@ android {
         buildConfig = true
     }
 }
-
 rust {
     rootDirRel = "../../../"
 }
-
 dependencies {
     implementation("androidx.webkit:webkit:1.14.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
@@ -68,5 +79,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
 }
-
 apply(from = "tauri.build.gradle.kts")
